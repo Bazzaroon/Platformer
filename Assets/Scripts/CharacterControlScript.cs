fd5c16fd608a0e9e9368835e8231c5431f499149
Bazzaroon <barry.tait@gmail.com>
2016-12-29 21:50:42 +0000

    Updates

--- Assets/Scripts/CharacterControlScript.cs
+++ Assets/Scripts/CharacterControlScript.cs
@@ -1,19 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CharacterControlScript : MonoBehaviour {
 
 	public float Speed;
 	public float VerticalSpeed;
-	private Animator anim;
 	public Rigidbody2D rb2d;
 	public Camera mainCamera;
-	private BoxCollider2D collider;
-	private CircleCollider2D grounder;
+	public float JumpHeight;
 	public float jumpForwardSpeed;
 
+	private Animator anim;
 	private bool jumping = false;
 	private bool facingForward = true;
+	private float jumpStart;
+	private BoxCollider2D collider;
+	private CircleCollider2D grounder;
+
 
 
 	// Use this for initialization
@@ -29,18 +32,26 @@ public class CharacterControlScript : MonoBehaviour {
 
 
 				var vRate = jumping ? 1f : 0f;
+		        
+				if (jumping) {
+						VerticalSpeed = VerticalSpeed / ((JumpHeight + jumpStart) + Mathf.Abs (jumpStart) / Mathf.Abs (transform.position.y));
+				}
+		//Debug.Log ("Jump Height:" + JumpHeight.ToString() + " Jump Start:" + jumpStart.ToString());
+
+				VerticalSpeed = VerticalSpeed < 1 ? 1 : VerticalSpeed;
 
 				var rate = jumping ? jumpForwardSpeed : Input.GetAxis ("Horizontal");
-
 				var tScale = transform.localScale;
 				var tForm = transform.position;
 				rb2d.velocity = new Vector2 (rate * Speed, vRate * VerticalSpeed);
 
 				if (rate < 0 && facingForward) {
 						transform.localScale = new Vector3 (-tScale.x, tScale.y);
+						jumpForwardSpeed = Mathf.Abs(jumpForwardSpeed);
 						facingForward = false;
 				} else if (rate > 0 && !facingForward) {
 						transform.localScale = new Vector3 (Mathf.Abs (tScale.x), tScale.y);
+						//jumpForwardSpeed = -jumpForwardSpeed;
 						facingForward = true;
 				}
 
@@ -50,13 +61,14 @@ public class CharacterControlScript : MonoBehaviour {
 				} else if (rate == 0) {
 						anim.SetBool ("Run", false);
 				}
-				if (transform.position.y > 1) {
-			jumping = false;
+				if (transform.position.y > jumpStart + JumpHeight) {
+					rb2d.gravityScale = 5;
+					jumping = false;
 				}
 
 		if (Input.GetKeyDown (KeyCode.Space))
 		{
-			rb2d.velocity = new Vector2(10f,10f);
+			jumpStart = transform.position.y;
 			jumping = true;
 		}
 	}
@@ -67,6 +79,7 @@ public class CharacterControlScript : MonoBehaviour {
 	{
 		if(coll.gameObject.tag == "grasstag")
 		{
+			VerticalSpeed = 20f;
 			jumping = false;
 		}
 	}
