c5ecd2f7f340172b7f5f00ac8ec81b8880bf13dd
Bazzaroon <barry.tait@gmail.com>
2016-12-30 22:28:42 +0000

    Scroller

--- Assets/Scripts/CharacterControlScript.cs
+++ Assets/Scripts/CharacterControlScript.cs
@@ -9,6 +9,7 @@ public class CharacterControlScript : MonoBehaviour {
 	public Camera mainCamera;
 	public float JumpHeight;
 	public float jumpForwardSpeed;
+	public float JumpSpeed;
 
 	private Animator anim;
 	private bool jumping = false;
@@ -16,6 +17,7 @@ public class CharacterControlScript : MonoBehaviour {
 	private float jumpStart;
 	private BoxCollider2D collider;
 	private CircleCollider2D grounder;
+	private float drag = 0f;
 
 
 
@@ -25,25 +27,29 @@ public class CharacterControlScript : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 		collider = mainCamera.GetComponent<BoxCollider2D> ();
 		grounder = GetComponentInChildren<CircleCollider2D> ();
+		//Position player in center of screen
+		transform.position = new Vector2 (0f, transform.position.y);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
 
-				var vRate = jumping ? 1f : 0f;
 		        
-				if (jumping) {
-						VerticalSpeed = VerticalSpeed / ((JumpHeight + jumpStart) + Mathf.Abs (jumpStart) / Mathf.Abs (transform.position.y));
-				}
-		Debug.Log ("Jump Height:" + JumpHeight.ToString() + " Jump Start:" + jumpStart.ToString() + " Vertical Speed:" + VerticalSpeed.ToString());
+		if (jumping)
+		{
+			rb2d.AddForce(new Vector2(0,JumpSpeed));
+			rb2d.drag = drag += 2;
+			anim.SetBool("Jump", true);
+		}
 
-				VerticalSpeed = VerticalSpeed < 1 ? 1 : VerticalSpeed;
+		var rate = Input.GetAxis ("Horizontal");
+		var tScale = transform.localScale;
+		var tForm = transform.position;
+		var cameraPos = mainCamera.transform.position;
 
-				var rate = jumping ? jumpForwardSpeed : Input.GetAxis ("Horizontal");
-				var tScale = transform.localScale;
-				var tForm = transform.position;
-				rb2d.velocity = new Vector2 (rate * Speed, vRate * VerticalSpeed);
+		mainCamera.transform.position  = new Vector3 (cameraPos.x , 0, 0);
+		//rb2d.velocity = new Vector2 (rate * Speed, 0);
 
 				if (rate < 0 && facingForward) {
 						transform.localScale = new Vector3 (-tScale.x, tScale.y);
@@ -55,32 +61,36 @@ public class CharacterControlScript : MonoBehaviour {
 						facingForward = true;
 				}
 
-
+		 
 				if (rate > 0 || rate < 0) {
 						anim.SetBool ("Run", true);
 				} else if (rate == 0) {
 						anim.SetBool ("Run", false);
 				}
-				if (transform.position.y > jumpStart + JumpHeight) {
-					rb2d.gravityScale = 5;
-					jumping = false;
-				}
-
-		if (Input.GetKeyDown (KeyCode.Space))
+				
+		if (transform.position.y > jumpStart + JumpHeight) 
 		{
-			jumpStart = transform.position.y;
-			jumping = true;
+			drag = 0;		
+			jumping = false;
 		}
+
 	}
 
+	void Update()
+	{
+		mainCamera.transform.Translate(new Vector3(-transform.position.x, mainCamera.transform.position.y, 0));
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			jumping = true;
+		}
+
+	}
 
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		if(coll.gameObject.tag == "grasstag")
-		{
-			VerticalSpeed = 20f;
-			jumping = false;
-		}
+		jumping = false;
+		anim.SetBool("Jump", false);
+		drag = 0;
 	}
 }
